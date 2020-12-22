960570bf1bfbaf08af80406c60f447cc960a2cbe
Zhouhao Rui <976667412@qq.com>
2020-12-22 20:42:36 +0000

    Week 9 Error HAndling

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -6,17 +6,23 @@ import {
 const router = express.Router(); // eslint-disable-line
 
 router.get('/', (req, res, next) => {
-	getMovies().then(movies => res.status(200).send(movies));
+	getMovies()
+	.then(movies => res.status(200).send(movies))
+	.catch(err => next(err));
 });
 
 router.get('/:id', (req, res, next) => {
 	const id = parseInt(req.params.id);
-	getMovie(id).then(movie => res.status(200).send(movie));
+	getMovie(id)
+	.then(movie => res.status(200).send(movie))
+	.catch(err => next(err));
 });
 
 router.get('/:id/reviews', (req, res, next) => {
   const id = parseInt(req.params.id);
-  getMovieReviews(id).then(reviews => res.status(200).send(reviews));
+	getMovieReviews(id)
+	.then(reviews => res.status(200).send(reviews))
+	.catch(err => next(err));
 });
 
 export default router;
