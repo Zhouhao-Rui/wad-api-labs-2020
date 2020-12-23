669d90181f39eda1f657ac4e4163a81fcc42c22d
Zhouhao Rui <976667412@qq.com>
2020-12-23 00:19:27 +0000

    Topic 10 - Session based Auth

--- movies-api/index.js
+++ movies-api/index.js
@@ -6,6 +6,8 @@ import './db';
 import { loadUsers } from './seedData';
 import usersRouter from './api/users';
 import GenresRouter from './api/genres';
+import session from 'express-session'
+import authenticate from './authenticate'
 
 dotenv.config();
 
@@ -24,13 +26,19 @@ const app = express();
 
 const port = process.env.PORT;
 
+app.use(session({
+  secret: 'ilikecake',
+  resave: true,
+  saveUninitialized: true
+}))
+
 //config body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
 app.use(express.static('public'));
 
-app.use('/api/movies', moviesRouter);
+app.use('/api/movies', authenticate, moviesRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/genres', GenresRouter);
 
